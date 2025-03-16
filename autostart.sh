b28509f7587a3ec32218eadf667a0e2f92130e3e
Fatih Yalçınkaya <96224317+FatihYalcinkaya@users.noreply.github.com>
2025-03-16 20:26:42 +0300

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+bash /home/fatih/.screenlayout/screenlayout.sh
+
+xfce4-clipman &
+xfce4-notifyd &
+slstatus &
+picom &
+blueman &
+nm-applet &
+blueman-applet &
+nitrogen --restore &
+libinput-gestures &
+xset -dpms &
+xset s off &
