a597a487739a49e8086db4cddcb86bd86b9516b8
Fatih Yalçınkaya <96224317+FatihYalcinkaya@users.noreply.github.com>
2025-05-26 16:05:43 +0300

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -113,6 +113,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_u,      spawn,          SHCMD("/usr/bin/displaySwitcher.sh") },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
