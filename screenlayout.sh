53a83895826a91177ae8eab6d9ef995fca1abc1a
Fatih Yalçınkaya <96224317+FatihYalcinkaya@users.noreply.github.com>
2025-05-26 16:06:20 +0300

    Delete screenlayout.sh

--- screenlayout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Bilgisayar ekranını ayarla
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
-
-# HDMI'yi aktif hale getir
-xrandr --output HDMI1 --mode 1920x1080 --rate 100 --pos 0x0 --rotate normal
